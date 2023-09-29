6917143715de212ad8e769c014d365864dd49682
Ivan <ivanwong810520@gmail.com>
2023-09-29 15:06:19 +0800

    Created WebSocket server
    - Established WebSocket server using the 'ws' library
    - Handle WebSocket connections, messages, and closures

--- server/server.js
+++ server/server.js
@@ -1,15 +1,38 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
 // Create an Express application
 const app = express();
 
+// Create an HTTP server using Express app
+const server = http.createServer(app);
+
+// Create a WebSocket server by attaching it to the HTTP server
+const wss = new WebSocket.Server({ server });
+
+// WebSocket server event handling
+wss.on("connection", (socket) => {
+    console.log("Connection started.");
+
+    // Handle incoming WebSocket messages
+    socket.on("message", (message) => {
+        console.log(`Incoming msg: ${message}`);
+    });
+
+    // Handle WebSocket connection closing
+    socket.on("close", () => {
+        console.log("Connection closed.");
+    });
+});
+
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
