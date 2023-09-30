c401e0905bafa4c0541baebe126aed3f47befe42
Ivan <ivanwong810520@gmail.com>
2023-10-01 07:57:35 +0800

    Added WebSocket client handling in client.js and Updated WebSocket server handling in server.js
    - Added WebSocket client event handling in client.js.
    - Updated WebSocket server handling for incoming messages from clients in server.js.
    - Added WebSocket close server event handling in server.js.

---
+++ client/client.js
@@ -0,0 +1,54 @@
+// Create a WebSocket connection to the server
+const socket = new WebSocket("ws://127.0.0.1:3000/server/");
+
+// Handle WebSocket connection events
+socket.onopen = () => {
+    document.getElementById("client__connection-status").innerHTML =
+        "Connected to Server";
+};
+
+socket.onclose = () => {
+    document.getElementById("client__connection-status").innerHTML =
+        "Disconnected to Server";
+};
+
+socket.onerror = (event) => {
+    document.getElementById(
+        "client__connection-status"
+    ).innerHTML = `Error Connecting to Server: ${event.message}`;
+};
+
+// Listen for messages from the server
+socket.onmessage = ({ data }) => {
+    console.log(`Message from server: ${data}`);
+    console.log(`Data type: ${typeof data}`);
+
+    // Create a new paragraph element to display the received message
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = data;
+    document.getElementById("client__messages").appendChild(paragraph);
+    document.getElementById("client__input-msg").value = "";
+};
+
+// Send a message to the server when the "Send" button is clicked
+document.getElementById("client__send-msg-btn").onclick = (e) => {
+    e.preventDefault();
+
+    const message = document.getElementById("client__input-msg").value.trim();
+
+    if (message) {
+        console.log(`Message to be send: ${message}`);
+        console.log(`Data type: ${typeof message}`);
+
+        socket.send(message);
+    } else {
+        alert(`Please input your message.`);
+    }
+};
+
+// Close the WebSocket connection when the "Close" button is clicked
+document.getElementById("client__close-connection-btn").onclick = (e) => {
+    e.preventDefault();
+
+    socket.close();
+};
